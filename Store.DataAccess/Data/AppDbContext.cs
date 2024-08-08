779c807e0c0f6e8a721ef03af4659642059094b3
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-08-08 15:41:38 +0300

    Add OrderHeader and OrderDetail Model

--- Store.DataAccess/Data/AppDbContext.cs
+++ Store.DataAccess/Data/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace Store.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<ShoppingCart> ShoppingCards { get; set; }
         public AppDbContext(DbContextOptions options) : base(options)
         {
