482f383fe656b9e9158699700f57ee6da8920ac6
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-08-12 04:35:53 +0300

    Handle showing number of products in shopping cart using viewComponent class

--- BookStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BookStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -28,7 +28,6 @@ namespace BookStore.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            HttpContext.Session.Clear();
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
