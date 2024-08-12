c2381b4e6989da1737741175ebbff71e577f3c98
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-08-12 04:20:20 +0300

    Update - Put the number or products in card when the user logout to zero using HttpContext.Session.Clear

--- BookStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BookStore/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -28,6 +28,7 @@ namespace BookStore.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            HttpContext.Session.Clear();
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
